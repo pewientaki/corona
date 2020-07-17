f184fada7fdfb02c82d19faab3b0471235faf560
Krystian Stec <krystianstec0@gmail.com>
2020-07-17 11:36:22 +0100

    build add

---
+++ build/precache-manifest.c6efc142d818c5a708dc1f9af7b561b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d6c0d9b1730a674589667e625dfc69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccbdf1e7773abd2007bd",
+    "url": "/static/css/main.d4c8c046.chunk.css"
+  },
+  {
+    "revision": "148183cded43846f5761",
+    "url": "/static/js/2.56941afa.chunk.js"
+  },
+  {
+    "revision": "98e7206d2472a9ba0c2c6593c9913f5f",
+    "url": "/static/js/2.56941afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccbdf1e7773abd2007bd",
+    "url": "/static/js/main.6fb2590e.chunk.js"
+  },
+  {
+    "revision": "a0a9cd5a916c40722b36",
+    "url": "/static/js/runtime-main.166ca98d.js"
+  },
+  {
+    "revision": "49e7e4df1587937441743e1c6db48777",
+    "url": "/static/media/corona-banner.49e7e4df.jpg"
+  }
+]);
